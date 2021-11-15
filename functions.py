460650ab75428cd975630aa5cec8d9db4ee94dab
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2021-11-15 07:16:05 -0500

    Se modificó función de tiempo para hacerla más concisa

--- functions.py
+++ functions.py
@@ -3,4 +3,5 @@ import time
 
 def has_time_passed(time_since, interval):
     # returns true if interval of time has passed since a specific time
-    return True if (time.time() - time_since) > interval else False
+    
+    return (time.time() - time_since) > interval
